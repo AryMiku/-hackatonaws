1cd195319ff8da1bf09b0de70c0952b8812dfcb1
wasupon <wasupon@builk.com>
2019-11-16 15:11:46 +0700

    เพิ่มมกล้อง

--- js/app.js
+++ js/app.js
@@ -4,6 +4,11 @@
 	var app = angular.module('application', []);
 	app.controller('storeController', function ($scope) {
 		$scope.sushis = "dfdfd"
+
+		$scope.take_snapshot = function(){
+			Webcam.snap( function(data_uri) {
+				document.getElementById('my_result').innerHTML = '<img src="'+data_uri+'"/>';
+			} );
+		}
 	});
-	
 })();
